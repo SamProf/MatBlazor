4b50f6bcf31a184247019a1065bad4e5ff1ac352
SamProf <wsamprof@gmail.com>
2018-10-26 20:54:10 +0200

    Let's start scss use

---
+++ src/MatBlazor.Web/gulpfile.js
@@ -0,0 +1,16 @@
+'use strict';
+
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+
+sass.compiler = require('node-sass');
+
+gulp.task('sass', function () {
+    return gulp.src('./sass/**/*.scss')
+        .pipe(sass().on('error', sass.logError))
+        .pipe(gulp.dest('./../MatBlazor/content'));
+});
+
+gulp.task('sass:watch', function () {
+    gulp.watch('./sass/**/*.scss', ['sass']);
+});
