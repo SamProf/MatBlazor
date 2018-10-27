038cccc2b8edd6fb4fe8c44968f0b3f7640ed5fd
SamProf <wsamprof@gmail.com>
2018-10-27 09:37:54 +0200

    MatCheckbox

---
+++ src/MatBlazor/Components/MatCheckbox/BaseMatCheckbox.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using MatBlazor.Components.Base;
+using Microsoft.AspNetCore.Blazor.Components;
+
+namespace MatBlazor.Components.MatCheckbox
+{
+    public class BaseMatCheckbox : BaseMatComponent
+    {
+        public override void UpdateComponent()
+        {
+        }
+        
+        [Parameter]
+        public bool Checked { get; set; }
+
+        [Parameter]
+        public bool Indeterminate { get; set; }
+
+        [Parameter]
+        public bool Disabled { get; set; }
+
+        [Parameter]
+        public string Value { get; set; }
+
+        public void ChangeHandler(object a)
+        {
+        }
+    }
+}
