73c6892991427dca037fe220786ec954efbcbefc
SamProf <wsamprof@gmail.com>
2018-10-30 18:22:01 +0100

    + BaseMatTab and MatTabGroup

---
+++ src/MatBlazor/Components/MatTab/BaseMatTab.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using MatBlazor.Components.Base;
+using Microsoft.AspNetCore.Blazor;
+using Microsoft.AspNetCore.Blazor.Components;
+
+namespace MatBlazor.Components.MatTab
+{
+    public class BaseMatTab : BaseMatComponent
+    {
+        [Parameter]
+        public RenderFragment ChildContent { get; set; }
+
+        public BaseMatTab()
+        {
+        }
+    }
+}
---
+++ src/MatBlazor/Components/MatTabGroup/BaseMatTabGroup.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using MatBlazor.Components.Base;
+
+namespace MatBlazor.Components.MatTabGroup
+{
+    public class BaseMatTabGroup : BaseMatComponent
+    {
+        public BaseMatTabGroup()
+        {
+        }
+    }
+}
