05bc755c25033cbd8a5fd98ea734ac7b9406a3e2
SamProf <wsamprof@gmail.com>
2018-11-01 11:02:40 +0100

    + MatSelect inital

---
+++ src/MatBlazor/Components/MatOption/BaseMatOption.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using MatBlazor.Components.Base;
+using Microsoft.AspNetCore.Blazor;
+using Microsoft.AspNetCore.Blazor.Components;
+
+namespace MatBlazor.Components.MatOption
+{
+    public class BaseMatOption : BlazorComponent
+    {
+        [Parameter]
+        protected RenderFragment ChildContent { get; set; }
+
+        [Parameter]
+        protected bool Disabled { get; set; }
+
+        [Parameter]
+        protected string Value { get; set; }
+    }
+}
---
+++ src/MatBlazor/Components/MatSelect/BaseMatSelect.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using MatBlazor.Components.Base;
+using Microsoft.AspNetCore.Blazor;
+using Microsoft.AspNetCore.Blazor.Components;
+
+namespace MatBlazor.Components.MatSelect
+{
+    public class BaseMatSelect : BaseMatComponent
+    {
+
+        protected ElementRef MdcSelectRef;
+
+        public BaseMatSelect()
+        {
+            ClassMapper.Add("mdc-select");
+        }
+
+        [Parameter]
+        protected RenderFragment ChildContent { get; set; }
+
+        [Parameter]
+        protected string Label { get; set; }
+
+
+        protected async override Task OnFirstAfterRenderAsync()
+        {
+            await base.OnFirstAfterRenderAsync();
+            await Js.InvokeAsync<object>("mdc.select.MDCSelect.attachTo", MdcSelectRef);
+        }
+    }
+}
