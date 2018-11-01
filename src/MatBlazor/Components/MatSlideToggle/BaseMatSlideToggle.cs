fa1a2120a6f06d6f933c05e635741a6bdd65d62a
SamProf <wsamprof@gmail.com>
2018-11-01 15:23:56 +0100

    + MatSlideToggle inital

---
+++ src/MatBlazor/Components/MatSlideToggle/BaseMatSlideToggle.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using MatBlazor.Components.Base;
+using Microsoft.AspNetCore.Blazor;
+using Microsoft.AspNetCore.Blazor.Components;
+
+namespace MatBlazor.Components.MatSlideToggle
+{
+    public class BaseMatSlideToggle : BaseMatComponent
+    {
+        protected ElementRef MdcSwitch;
+        private bool _checked;
+
+
+        protected void OnChangedHandler(UIChangeEventArgs e)
+        {
+            Checked = (bool) e.Value;
+        }
+
+        [Parameter]
+        public Action<bool> CheckedChanged { get; set; }
+
+        [Parameter]
+        public string Label { get; set; }
+
+        [Parameter]
+        public bool Checked
+        {
+            get => _checked;
+            set
+            {
+                if (_checked != value)
+                {
+                    _checked = value;
+                    CheckedChanged?.Invoke(value);
+                }
+            }
+        }
+
+        protected async override Task OnFirstAfterRenderAsync()
+        {
+            await base.OnFirstAfterRenderAsync();
+            await Js.InvokeAsync<object>("mdc.switchControl.MDCSwitch.attachTo", MdcSwitch);
+        }
+    }
+}
