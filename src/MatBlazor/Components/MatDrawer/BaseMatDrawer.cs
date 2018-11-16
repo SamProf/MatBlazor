62cd63ac6279bb8d2ff1c4867a9fd87c9d1f898c
SamProf <wsamprof@gmail.com>
2018-11-16 21:35:58 +0100

    Upgrade to Blazor 0.7.0, Drawer in progress

--- src/MatBlazor/Components/MatDrawer/BaseMatDrawer.cs
+++ src/MatBlazor/Components/MatDrawer/BaseMatDrawer.cs
@@ -3,11 +3,17 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using MatBlazor.Components.Base;
+using Microsoft.AspNetCore.Blazor;
+using Microsoft.AspNetCore.Blazor.Components;
 
 namespace MatBlazor.Components.MatDrawer
 {
     public class BaseMatDrawer : BaseMatComponent
     {
+
+        [Parameter]
+        protected RenderFragment ChildContent { get; set; }
+
         public BaseMatDrawer()
         {
             ClassMapper
