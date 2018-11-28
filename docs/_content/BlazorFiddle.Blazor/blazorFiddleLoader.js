1b4ff2712414bc5b8d40e4a4e9b9a95e20b66eff
SamProf <wsamprof@gmail.com>
2018-11-28 23:58:13 +0100

    https

--- docs/_content/BlazorFiddle.Blazor/blazorFiddleLoader.js
+++ docs/_content/BlazorFiddle.Blazor/blazorFiddleLoader.js
@@ -2,7 +2,7 @@
 
 
     var script = document.createElement("script"); // create a script DOM node
-    script.src = "http://blazorfiddle.com/scripts/blazorFiddle.js"; // set its src to the provided URL
+    script.src = "https://blazorfiddle.com/scripts/blazorFiddle.js"; // set its src to the provided URL
 
     document.head.appendChild(script);
 
