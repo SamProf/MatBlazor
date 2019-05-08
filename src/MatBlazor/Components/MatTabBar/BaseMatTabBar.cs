6d449cf1f5d3e3ce632bea0d67ac9a4eb4913875
Vladimir Samoilenko <wsamprof@gmail.com>
2019-05-08 23:08:46 +0200

    in progress MatTabBar

--- src/MatBlazor/Components/MatTabBar/BaseMatTabBar.cs
+++ src/MatBlazor/Components/MatTabBar/BaseMatTabBar.cs
@@ -1,6 +1,15 @@
-﻿namespace MatBlazor
+﻿using Microsoft.AspNetCore.Components;
+
+namespace MatBlazor
 {
     public class BaseMatTabBar : BaseMatComponent
     {
+        [Parameter]
+        protected RenderFragment ChildContent { get; set; }
+
+        public BaseMatTabBar()
+        {
+            ClassMapper.Add("mdc-tab-bar");
+        }
     }
 }
--- src/MatBlazor/Components/MatTabLabel/BaseMatTabLabel.cs
+++ src/MatBlazor/Components/MatTabLabel/BaseMatTabLabel.cs
@@ -1,6 +1,18 @@
-﻿namespace MatBlazor
+﻿using Microsoft.AspNetCore.Components;
+
+namespace MatBlazor
 {
     public class BaseMatTabLabel : BaseMatComponent
     {
+        [Parameter]
+        protected RenderFragment ChildContent { get; set; }
+
+        [CascadingParameter]
+        protected BaseMatTabBar Parent { get; set; }
+
+        public BaseMatTabLabel()
+        {
+            ClassMapper.Add("mdc-tab");
+        }
     }
 }
