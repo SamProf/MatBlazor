ce2c433f2b2aaa056c1ea1cea5df2d2772868e51
Vladimir Samoilenko <wsamprof@gmail.com>
2019-05-10 23:05:44 +0200

    DatePicker Proof of concept

---
+++ src/MatBlazor/Components/MatDatePicker/BaseMatDatePicker.cs
@@ -0,0 +1,12 @@
+﻿using System.Threading.Tasks;
+
+namespace MatBlazor
+{
+    public class BaseMatDatePicker : BaseMatComponent
+    {
+        public BaseMatDatePicker()
+        {
+            CallAfterRender(async () => { Js.InvokeAsync<object>("matBlazor.matDatePicker.init", Ref); });
+        }
+    }
+}
