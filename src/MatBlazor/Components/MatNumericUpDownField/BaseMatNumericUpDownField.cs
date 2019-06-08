f46651c803911843bfe54fca43dc8dcf2dd68c9b
dani herrera <ctrl.alt.d@gmail.com>
2019-06-08 17:39:26 +0200

    Improve mat design label

--- src/MatBlazor/Components/MatNumericUpDownField/BaseMatNumericUpDownField.cs
+++ src/MatBlazor/Components/MatNumericUpDownField/BaseMatNumericUpDownField.cs
@@ -293,6 +293,7 @@ namespace MatBlazor
 
             LabelClassMapper
                 .Add("mdc-floating-label")
+                .If("mat-floating-label--float-above-outlined", () => Outlined && !string.IsNullOrEmpty(UiBindedValue) )
                 .If("mdc-floating-label--float-above", () => !string.IsNullOrEmpty(UiBindedValue));
 
             InputClassMapper
