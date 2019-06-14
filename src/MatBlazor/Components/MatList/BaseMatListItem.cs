e4fb2f50b42e8ede3c7a6305f9fd6f22169994b6
enkodellc <keith@keithfimreite.com>
2019-06-14 11:56:43 -0700

    MatListItem Disabled

---
+++ src/MatBlazor/Components/MatList/BaseMatListItem.cs
@@ -0,0 +1,50 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Components;
+
+namespace MatBlazor
+{
+    /// <summary>
+    /// Lists present multiple line items vertically as a single continuous element. 
+    /// </summary>
+    public class BaseMatListItem : BaseMatDomComponent
+    {
+        [Parameter]
+        protected EventCallback<UIMouseEventArgs> OnMouseDown { get; set; }
+
+        [Parameter]
+        protected EventCallback<UIMouseEventArgs> OnClick { get; set; }
+
+        [Parameter]
+        protected RenderFragment ChildContent { get; set; }
+
+        [Parameter]
+        protected string Href { get; set; }
+
+        /// <summary>
+        /// List Item is disabled.
+        /// </summary>
+        [Parameter]
+        public bool Disabled
+        {
+            get => _disabled;
+            set
+            {
+                _disabled = value;
+                if (_disabled)
+                {
+
+                }
+                ClassMapper.MakeDirty();
+            }
+        }
+
+        private bool _disabled;
+
+        public BaseMatListItem()
+        {
+            ClassMapper
+                .Add("mdc-list-item")
+                .If("mdc-list-item--disabled", () => _disabled);
+        }
+    }
+}
