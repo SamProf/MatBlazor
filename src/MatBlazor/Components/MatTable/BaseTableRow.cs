d53a65916712bfa89554beaa156135d1a0707f55
enkodellc <keith@keithfimreite.com>
2019-06-28 21:46:27 -0700

    #89 Table Row Select, Hover, RowClass, HeaderRowClass

---
+++ src/MatBlazor/Components/MatTable/BaseTableRow.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Components;
+
+namespace MatBlazor
+{
+    /// <summary>
+    /// Mat Table Row display a table row   
+    /// </summary>
+    public class BaseTableRow : BaseMatDomComponent
+    {
+        [CascadingParameter]
+        public BaseMatTable Table { get; set; }
+
+        [Parameter]
+        public RenderFragment ChildContent { get; set; }
+
+        [Parameter]
+        public bool Selected { get; set; }
+
+        [Parameter]
+        public bool AllowSelection { get; set; }
+
+        [Parameter]
+        public EventCallback<bool> SelectedChanged { get; set; }
+
+        public async Task ToggleSelectedAsync()
+        {
+            this.Selected = !this.Selected;
+            ClassMapper.MakeDirty();
+            await SelectedChanged.InvokeAsync(this.Selected);
+            await this.Table.ToggleSelectedAsync(this);
+            this.StateHasChanged();            
+        }
+
+        public BaseTableRow()
+        {
+            ClassMapper
+                .Add("mdc-table-row")
+                .If("mdc-table-row-hover", () => AllowSelection)
+                .If("mdc-table-row-selected", () => Selected);
+        }
+
+        protected async void OnClickHandler(UIMouseEventArgs e)
+        {
+            if (this.AllowSelection)
+            {
+                await this.ToggleSelectedAsync();
+            }
+        }
+    }
+}
