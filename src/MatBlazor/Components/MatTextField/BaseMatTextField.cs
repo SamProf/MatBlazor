4d3ec74bb14f10faabc25c218d1544280651a823
katiep23 <51712098+katiep23@users.noreply.github.com>
2019-07-26 15:16:48 -0500

    yup

--- src/MatBlazor/Components/MatTextField/BaseMatTextField.cs
+++ src/MatBlazor/Components/MatTextField/BaseMatTextField.cs
@@ -54,9 +54,6 @@ namespace MatBlazor
         public string Label { get; set; }
 
         [Parameter]
-        public string Id {get; set;}
-
-        [Parameter]
         public string Icon { get; set; }
 
         [Parameter]
