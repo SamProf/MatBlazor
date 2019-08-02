51ea05e35828d01f7ecab322e487e2be03634c08
enkodellc <keith@keithfimreite.com>
2019-08-02 15:58:40 -0700

    MatNavMenu - Initial

---
+++ src/MatBlazor/Components/MatNavMenu/BaseMatNavMenu.cs
@@ -0,0 +1,38 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Components;
+
+namespace MatBlazor
+{
+    public class BaseMatNavMenu : BaseMatDomComponent
+    {
+        [Parameter]
+        public RenderFragment ChildContent { get; set; }
+
+        [Parameter]
+        public bool Multi { get; set; }
+
+        public BaseMatNavSubMenu Current { get; private set; }
+
+        public async Task ToggleAsync(BaseMatNavSubMenu subMenu)
+        {
+            if (!Multi)
+            {
+                if (subMenu.Expanded)
+                {
+                    var current = Current;
+                    Current = subMenu;
+
+                    if (current != null && current != subMenu && current.Expanded)
+                    {
+                        await current.ToggleAsync();
+                    }
+                }
+            }
+        }
+
+        public BaseMatNavMenu()
+        {
+            ClassMapper.Add("mat-accordion");
+        }
+    }
+}
---
+++ src/MatBlazor/Components/MatNavMenu/BaseMatNavSubMenu.cs
@@ -0,0 +1,38 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Components;
+
+namespace MatBlazor
+{
+    /// <summary>
+    /// MatExpansionPanel provides an expandable details-summary view.
+    /// </summary>
+    public class BaseMatNavSubMenu : BaseMatDomComponent
+    {
+        [CascadingParameter]
+        public BaseMatNavMenu MatNavMenu { get; set; }
+
+        [Parameter]
+        public RenderFragment ChildContent { get; set; }
+
+        [Parameter]
+        public bool Expanded { get; set; }
+
+        [Parameter]
+        public EventCallback<bool> ExpandedChanged { get; set; }
+    
+        public async Task ToggleAsync()
+        {
+            this.Expanded = !this.Expanded;
+            await ExpandedChanged.InvokeAsync(this.Expanded);
+            await this.MatNavMenu.ToggleAsync(this);
+            this.StateHasChanged();
+        }
+
+        public BaseMatNavSubMenu()
+        {
+            ClassMapper
+                .Add("mat-expansion-panel")
+                .If("mat-expansion-panel-expanded", () => Expanded);
+        }
+    }
+}
