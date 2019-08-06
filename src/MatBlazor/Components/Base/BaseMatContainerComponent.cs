239fea67d34695825ec99142077eb46be77a7e61
Vladimir Samoilenko <wsamprof@gmail.com>
2019-08-06 21:45:22 +0200

    MatCardActionButtons

---
+++ src/MatBlazor/Components/Base/BaseMatContainerComponent.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.RenderTree;
+
+namespace MatBlazor
+{
+    public abstract class BaseMatContainerComponent : BaseMatDomComponent
+    {
+        [Parameter]
+        public RenderFragment ChildContent { get; set; }
+
+        [Parameter]
+        public string Tag { get; set; } = "div";
+
+        protected override void BuildRenderTree(RenderTreeBuilder builder)
+        {
+            builder.OpenElement(0, Tag);
+            builder.AddAttribute(1, "class", ClassMapper.Class);
+            builder.AddAttribute(2, "style", GenerateStyle());
+            builder.AddMultipleAttributes(3,
+                RuntimeHelpers
+                    .TypeCheck<global::System.Collections.Generic.IEnumerable<
+                        global::System.Collections.Generic.KeyValuePair<string, object>>>(Attributes));
+            builder.AddAttribute(4, "Id", Id);
+            builder.AddElementReferenceCapture(5, (__value) => { Ref = __value; });
+            builder.AddContent(7, ChildContent);
+            builder.CloseElement();
+        }
+    }
+}
