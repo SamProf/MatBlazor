0321a95a1ea4ae407299fe05df2abde2179aa3d1
SamProf <wsamprof@gmail.com>
2020-02-03 22:30:35 +0100

    Fist implementation of MatDataTable

--- src/MatBlazor/Components/MatDataTable/BaseMatDataTable.cs
+++ src/MatBlazor/Components/MatDataTable/BaseMatDataTable.cs
@@ -5,6 +5,17 @@ namespace MatBlazor
 {
     public class BaseMatDataTable<TItem> : BaseMatDomComponent
     {
+        public BaseMatDataTable()
+        {
+            ClassMapper
+                .Add("mat-data-table")
+                .Add("mdc-data-table");
+        }
+
+
+        [Parameter]
+        public RenderFragment ChildContent { get; set; }
+
         [Parameter]
         public IReadOnlyList<TItem> Items { get; set; }
     }
