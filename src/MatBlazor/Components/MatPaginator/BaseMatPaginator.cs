aa1367a00062c00b99bcd508f96e7d4ebd5c4657
SamProf <wsamprof@gmail.com>
2020-02-09 14:16:10 +0100

    MatPaginator

--- src/MatBlazor/Components/MatPaginator/BaseMatPaginator.cs
+++ src/MatBlazor/Components/MatPaginator/BaseMatPaginator.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections.Generic;
 using System.IO;
+using System.Threading.Tasks;
 using Microsoft.AspNetCore.Components;
 
 namespace MatBlazor
@@ -8,15 +9,39 @@ namespace MatBlazor
     {
         public string Label { get; set; } = "Items per Page:";
 
+
         [Parameter]
-        public IReadOnlyList<MatPageSize> PageSizes { get; set; } = new[]
+        public int PageSize { get; set; }
+
+        [Parameter]
+        public EventCallback<int> PageSizeChanged { get; set; }
+
+
+        [Parameter]
+        public int PageIndex { get; set; }
+
+        [Parameter]
+        public EventCallback<int> PageIndexChanged { get; set; }
+
+        [Parameter]
+        public int Length { get; set; }
+
+        [Parameter]
+        public IReadOnlyList<MatPageSizeOption> PageSizeOptions { get; set; } = new[]
         {
-            new MatPageSize(5),
-            new MatPageSize(10),
-            new MatPageSize(25),
-            new MatPageSize(50),
-            new MatPageSize(100),
-            new MatPageSize(-1, "*"),
+            new MatPageSizeOption(5),
+            new MatPageSizeOption(10),
+            new MatPageSizeOption(25),
+            new MatPageSizeOption(50),
+            new MatPageSizeOption(100),
+            new MatPageSizeOption(-1, "*"),
         };
+
+
+        protected async Task PageSizeChangedHandler(int value)
+        {
+            this.PageSize = value;
+            await this.PageSizeChanged.InvokeAsync(value);
+        }
     }
 }
---
+++ src/MatBlazor/Components/MatPaginator/MatPageSizeOption.cs
@@ -0,0 +1,23 @@
+﻿namespace MatBlazor
+{
+    public class MatPageSizeOption
+    {
+        public int Value { get; set; }
+        public string Text { get; set; }
+
+        public override string ToString()
+        {
+            return Text;
+        }
+
+        public MatPageSizeOption(int value, string text)
+        {
+            Value = value;
+            Text = text;
+        }
+
+        public MatPageSizeOption(int value) : this(value, value.ToString())
+        {
+        }
+    }
+}
