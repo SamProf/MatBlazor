c4b1cf959c72393ba695426e2807d396fe1aef1d
SamProf <wsamprof@gmail.com>
2020-02-09 13:46:06 +0100

    Prepare for MatPaginator

---
+++ src/MatBlazor/Components/MatPaginator/BaseMatPaginator.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using Microsoft.AspNetCore.Components;
+
+namespace MatBlazor
+{
+    public class BaseMatPaginator : BaseMatDomComponent
+    {
+        public string Label { get; set; } = "Items per Page:";
+
+        [Parameter]
+        public IReadOnlyList<MatPageSize> PageSizes { get; set; } = new[]
+        {
+            new MatPageSize(5),
+            new MatPageSize(10),
+            new MatPageSize(25),
+            new MatPageSize(50),
+            new MatPageSize(100),
+            new MatPageSize(-1, "*"),
+        };
+    }
+}
---
+++ src/MatBlazor/Components/MatPaginator/MatPageSize.cs
@@ -0,0 +1,18 @@
+﻿namespace MatBlazor
+{
+    public class MatPageSize
+    {
+        public long Value { get; set; }
+        public string Text { get; set; }
+
+        public MatPageSize(long value, string text)
+        {
+            Value = value;
+            Text = text;
+        }
+
+        public MatPageSize(long value) : this(value, value.ToString())
+        {
+        }
+    }
+}
