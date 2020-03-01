a47da8e4c3c0ed1ea629ce67c887b1fcee12f46c
SamProf <wsamprof@gmail.com>
2020-03-01 14:49:12 +0100

    VirtualScroll Disabled mode

--- src/MatBlazor/Components/MatVirtualScroll/BaseMatVirtualScroll.cs
+++ src/MatBlazor/Components/MatVirtualScroll/BaseMatVirtualScroll.cs
@@ -11,33 +11,38 @@ namespace MatBlazor
     /// The VirtualScroll for Blazor displays large lists of elements performantly by only rendering the items that fit on-screen.
     /// Loading hundreds of elements can be slow in any browser; virtual scrolling enables a performant way to simulate all items being rendered by making the height of the container element the same as the height of total number of elements to be rendered, and then only rendering the items in view.
     /// </summary>
-    /// <typeparam name="ItemType"></typeparam>
-    public class BaseMatVirtualScroll<ItemType> : BaseMatDomComponent, IMatVirtualScroll
+    /// <typeparam name="TItem"></typeparam>
+    public class BaseMatVirtualScroll<TItem> : BaseMatDomComponent, IMatVirtualScroll
     {
         [Parameter]
         public int ItemHeight { get; set; } = 50;
 
+        [Parameter]
+        public bool Disabled { get; set; }
 
         [Parameter]
-        public RenderFragment<ItemType> ChildContent { get; set; }
+        public RenderFragment<TItem> ChildContent { get; set; }
 
         [Parameter]
-        public IEnumerable<ItemType> Items { get; set; }
+        public IEnumerable<TItem> Items { get; set; }
 
         public MatDotNetObjectReference<MatVirtualScrollJsHelper> JsHelperReference { get; set; }
 
 
         public BaseMatVirtualScroll()
         {
-
             ClassMapper.Add("﻿mat-virtual-scroll");
-            
+
             CallAfterRender(async () =>
             {
-                JsHelperReference = new MatDotNetObjectReference<MatVirtualScrollJsHelper>(new MatVirtualScrollJsHelper(this));
-                var scrollView = await Js.InvokeAsync<MatVirtualScrollView>("matBlazor.matVirtualScroll.init", Ref,
-                    JsHelperReference.Reference);
-                this.SetScrollView(scrollView);
+                if (!Disabled)
+                {
+                    JsHelperReference =
+                        new MatDotNetObjectReference<MatVirtualScrollJsHelper>(new MatVirtualScrollJsHelper(this));
+                    var scrollView = await Js.InvokeAsync<MatVirtualScrollView>("matBlazor.matVirtualScroll.init", Ref,
+                        JsHelperReference.Reference);
+                    this.SetScrollView(scrollView);
+                }
             });
         }
 
@@ -52,6 +57,29 @@ namespace MatBlazor
             base.Dispose();
             // todo call js to dispose events (window resize)
             JsHelperReference?.Dispose();
+        }
+
+
+        protected string GetContentStyle()
+        {
+            if (Disabled)
+            {
+                return $"height: {Items.Count() * ItemHeight}px;";
+            }
+
+            return
+                $"height: {(ScrollViewResult.Height - ScrollViewResult.SkipItems * ItemHeight)}px; padding-top: {(ScrollViewResult.SkipItems * ItemHeight)}px;";
+        }
+
+        protected IEnumerable<TItem> GetContentItems()
+        {
+            if (Disabled)
+            {
+                return Items;
+            }
+
+            return
+                Items.Skip(ScrollViewResult.SkipItems).Take(ScrollViewResult.TakeItems);
         }
 
         private void SetScrollView(MatVirtualScrollView scrollView)
