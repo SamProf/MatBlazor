9e14df421687ed589e9a808dba0afea0cc703a28
Damiano Gambarotto <damiano.g@outlook.it>
2020-10-17 10:21:27 +0200

    MatAnchor code cleanup

---
+++ src/MatBlazor/Components/MatAnchor/MatAnchorContainer.razor.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MatBlazor
+{
+    partial class MatAnchorContainer
+    {
+        [Inject]
+        private NavigationManager NavigationManager { get; set; }
+
+        [Parameter(CaptureUnmatchedValues = true)]
+        public Dictionary<string, object> Attributes { get; set; }
+
+        [Parameter]
+        public RenderFragment ChildContent { get; set; }
+
+        [Parameter]
+        public string Anchor { get; set; }
+
+        string GetHref()
+        {
+            return NavigationManager.ToAbsoluteUri(NavigationManager.Uri).GetLeftPart(UriPartial.Path) + "#" + Anchor;
+        }
+    }
+}
---
+++ src/MatBlazor/Components/MatAnchor/MatAnchorLink.razor.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+
+namespace MatBlazor
+{
+    partial class MatAnchorLink
+    {
+        [Inject]
+        NavigationManager NavigationManager { get; set; }
+
+        [Parameter(CaptureUnmatchedValues = true)]
+        public Dictionary<string, object> Attributes { get; set; }
+
+        [Parameter]
+        public RenderFragment ChildContent { get; set; }
+
+        string GetHref()
+        {
+            var hrefValue = Attributes["href"].ToString();
+            if (hrefValue.StartsWith("#"))
+            {
+                hrefValue = NavigationManager.ToAbsoluteUri(NavigationManager.Uri).GetLeftPart(UriPartial.Path) + hrefValue;
+            }
+            return hrefValue;
+        }
+    }
+}
