65c992100486361a0a8759255753c6ce2f10f24e
Damiano <damiano.g@outlook.it>
2020-10-21 00:36:10 +0200

    Accordion Cleanup code and removed redundant base class

---
+++ src/MatBlazor/Components/MatAccordion/MatExpansionPanelSubHeader.razor.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace MatBlazor
+{
+    partial class MatExpansionPanelSubHeader
+    {
+        [Parameter]
+        public RenderFragment ChildContent { get; set; }
+    }
+}
