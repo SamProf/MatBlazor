1d1d3a1a29a14834e8060f2e1ad8a61006c8ec75
Damiano Gambarotto <damiano.g@outlook.it>
2020-10-21 08:46:05 +0200

    MatAppBar... code cleanup and removed redundant base class

---
+++ src/MatBlazor/Components/MatAppBar/MatAppBarNavigationIcon.razor.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Web;
+
+namespace MatBlazor
+{
+    partial class MatAppBarNavigationIcon
+    {
+        [Parameter]
+        public EventCallback<MouseEventArgs> OnClick { get; set; }
+
+        void OnClickHandler(MouseEventArgs e)
+        {
+            OnClick.InvokeAsync(e);
+        }
+    }
+}
