532a2eef576d3c78e1bbb1e64e5d5ce890d21eb4
Gweana <adamek.stepan@gmail.com>
2020-10-29 16:42:25 +0100

    Fix NumericUpDown step value

--- src/MatBlazor/Components/MatNumericUpDownField/BaseMatNumericUpDownFieldInternal.cs
+++ src/MatBlazor/Components/MatNumericUpDownField/BaseMatNumericUpDownFieldInternal.cs
@@ -104,7 +104,7 @@ namespace MatBlazor
                 }
                 Format = $"p{DecimalPlaces}";
             }
-            else
+            else if (Step == null || Step.Equals(ZeroValue))
             {
                 Step = SwitchT.GetStep();
             }
