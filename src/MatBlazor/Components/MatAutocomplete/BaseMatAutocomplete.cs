8edb67f50170eb64a8dbabde5b00935410faf608
Sandro Hanea <sandro@shane.ro>
2021-01-13 10:02:30 +0200

    Removed some unused namespace

--- src/MatBlazor/Components/MatAutocomplete/BaseMatAutocomplete.cs
+++ src/MatBlazor/Components/MatAutocomplete/BaseMatAutocomplete.cs
@@ -4,7 +4,6 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using MatBlazor.Components.MatAutocomplete;
-using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Components.Web;
 
 namespace MatBlazor
