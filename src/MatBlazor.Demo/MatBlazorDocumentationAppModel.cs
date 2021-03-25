541088600300c7bee422b0466cf17daceaff56b9
TomSun <tftomsun@web.de>
2021-03-25 20:00:44 +0100

    made appbar links configurable

---
+++ src/MatBlazor.Demo/MatBlazorDocumentationAppModel.cs
@@ -0,0 +1,392 @@
+﻿using MatBlazor.Demo.Models;
+using MatBlazor.Demo.Pages;
+using System;
+using System.Linq;
+
+namespace MatBlazor.Demo
+{
+    public class MatBlazorDocumentationAppModel : AppModel
+    {
+        public MatBlazorDocumentationAppModel() : base(typeof(MatBlazorDocIndex), GetNavModel(), true, FilterType)
+        {
+        }
+        private static bool FilterType(Type componentType)
+        {
+
+            if (componentType.Name.StartsWith("Base") || componentType.Name.EndsWith("Internal") || componentType.Name.EndsWith("Internal`1"))
+            {
+                return false;
+            }
+
+
+            if (componentType == typeof(MatSelectItem<>))
+            {
+
+            }
+
+            return true;
+
+        }
+        private static NavModel GetNavModel()
+        {
+            NavItem[] navItems = CreateNavItems();
+
+            var groupModels = navItems
+                .GroupBy(i => i.Group)
+                .OrderBy(i => i.Key.Order)
+                .ThenBy(i => i.Key.Name)
+                .Select(g =>
+                {
+                    return new NavGroupModel()
+                    {
+                        Group = g.Key,
+                        Items = g
+                            .OrderBy(i => i.Order)
+                            .ThenBy(i => i.Name)
+                            .ToArray(),
+                    };
+                })
+                .ToArray();
+
+            var model = new NavModel(
+                "MatBlazor - Material Design components for Blazor and Razor Components",
+                ("http://samprof.com", "Samprof.com", "user-circle"),
+                ("https://github.com/SamProf/MatBlazor", "Github", "github"));
+            model.NavGroups = groupModels;
+            return model;
+        }
+
+        private static NavItem[] CreateNavItems()
+        {
+            var groups = new
+            {
+                FormControls = new NavGroup("Form Controls", 1),
+                Navigation = new NavGroup("Navigation", 2),
+                Layout = new NavGroup("Layout", 3),
+                ButtonsAndIndicators = new NavGroup("Buttons & Indicators", 4),
+                PopupsAndModals = new NavGroup("Popups & Modals", 5),
+                DataTable = new NavGroup("Data Table", 6),
+                Other = new NavGroup("Other", 10),
+            };
+            var navItems = new[]
+            {
+            new NavItem()
+            {
+                Name = "Validation via EditContext",
+                Url = "EditContext",
+                Group = groups.FormControls,
+                Order = 1
+            },
+                new NavItem()
+                {
+                    Name = "Autocomplete",
+                    Url = "Autocomplete",
+                    Group = groups.FormControls,
+                },
+            new NavItem()
+            {
+                Name = "AutocompleteList",
+                Url = "AutocompleteList",
+                Group = groups.FormControls,
+            },
+            new NavItem()
+            {
+                Name = "Checkbox",
+                Url = "Checkbox",
+                Group = groups.FormControls,
+            },
+            new NavItem()
+            {
+                Name = "TextField",
+                Url = "TextField",
+                Group = groups.FormControls,
+            },
+            new NavItem()
+            {
+                Name = "RadioButton",
+                Url = "RadioButton",
+                Group = groups.FormControls,
+            },
+            new NavItem()
+            {
+                Name = "Select",
+                Url = "Select",
+                Group = groups.FormControls,
+            },
+            new NavItem()
+            {
+                Name = "SelectItem",
+                Url = "SelectItem",
+                Group = groups.FormControls,
+            },
+            new NavItem()
+            {
+                Name = "SelectValue",
+                Url = "SelectValue",
+                Group = groups.FormControls,
+            },
+            new NavItem()
+            {
+                Name = "Slider",
+                Url = "Slider",
+                Group = groups.FormControls,
+            },
+            new NavItem()
+            {
+                Name = "SlideToggle",
+                Url = "SlideToggle",
+                Group = groups.FormControls,
+            },
+            new NavItem()
+            {
+                Name = "Drawer",
+                Url = "Drawer",
+                Group = groups.Navigation
+            },
+            new NavItem()
+            {
+                Name = "AppBar",
+                Url = "AppBar",
+                Group = groups.Navigation
+            },
+            new NavItem()
+            {
+                Name = "ButtonLink",
+                Url = "ButtonLink",
+                Group = groups.Navigation
+            },
+            new NavItem()
+            {
+                Name = "Dialog",
+                Url = "Dialog",
+                Group = groups.PopupsAndModals
+            },
+            new NavItem()
+            {
+                Name = "Snackbar",
+                Url = "Snackbar",
+                Group = groups.PopupsAndModals
+            },
+            new NavItem()
+            {
+                Name = "Menu",
+                Url = "Menu",
+                Group = groups.Layout
+            },
+            new NavItem()
+            {
+                Name = "Card",
+                Url = "Card",
+                Group = groups.Layout
+            },
+            new NavItem()
+            {
+                Name = "Divider",
+                Url = "Divider",
+                Group = groups.Layout
+            },
+            new NavItem()
+            {
+                Name = "List",
+                Url = "List",
+                Group = groups.Layout
+            },
+            new NavItem()
+            {
+                Name = "TreeView",
+                Url = "TreeView",
+                Group = groups.Layout
+            },
+            new NavItem()
+            {
+                Name = "Button",
+                Url = "Button",
+                Group = groups.ButtonsAndIndicators
+            },
+            new NavItem()
+            {
+                Name = "Ripple",
+                Url = "Ripple",
+                Group = groups.ButtonsAndIndicators
+            },
+            new NavItem()
+            {
+                Name = "IconButton",
+                Url = "IconButton",
+                Group = groups.ButtonsAndIndicators
+            },
+            new NavItem()
+            {
+                Name = "Icon",
+                Url = "Icon",
+                Group = groups.ButtonsAndIndicators
+            },
+            new NavItem()
+            {
+                Name = "Chip",
+                Url = "Chip",
+                Group = groups.ButtonsAndIndicators
+            },
+            new NavItem()
+            {
+                Name = "ProgressBar",
+                Url = "ProgressBar",
+                Group = groups.ButtonsAndIndicators
+            },
+            new NavItem()
+            {
+                Name = "ProgressCircle",
+                Url = "ProgressCircle",
+                Group = groups.ButtonsAndIndicators
+            },
+            new NavItem()
+            {
+                Name = "Paper & Elevation",
+                Url = "Paper",
+                Group = groups.Layout
+            },
+            new NavItem()
+            {
+                Name = "Typography",
+                Url = "Typography",
+                Group = groups.Layout
+            },
+            new NavItem()
+            {
+                Name = "Layout Grid",
+                Url = "LayoutGrid",
+                Group = groups.Layout
+            },
+            new NavItem()
+            {
+                Name = "Tab",
+                Url = "Tab",
+                Group = groups.Layout
+            },
+            new NavItem()
+            {
+                Name = "DatePicker",
+                Url = "DatePicker",
+                Group = groups.FormControls
+            },
+            new NavItem()
+            {
+                Name = "FileUpload",
+                Url = "FileUpload",
+                Group = groups.FormControls
+            },
+            new NavItem()
+            {
+                Name = "Themes",
+                Url = "Themes",
+                Group = groups.Layout
+            },
+            new NavItem()
+            {
+                Name = "FAB",
+                Url = "FAB",
+                Group = groups.ButtonsAndIndicators
+            },
+            new NavItem()
+            {
+                Name = "Expansion Panel",
+                Url = "ExpansionPanel",
+                Group = groups.Layout
+            },
+            new NavItem()
+            {
+                Name = "Toast",
+                Url = "Toast",
+                Group = groups.PopupsAndModals
+            },
+            new NavItem()
+            {
+                Name = "Tooltip",
+                Url = "Tooltip",
+                Group = groups.PopupsAndModals
+            },
+            new NavItem()
+            {
+                Name = "Hidden",
+                Url = "Hidden",
+                Group = groups.Layout
+            },
+            new NavItem()
+            {
+                Name = "NumericUpDownField",
+                Url = "NumericUpDownField",
+                Group = groups.FormControls
+            },
+            new NavItem()
+            {
+                Name = "Nav Menu",
+                Url = "NavMenu",
+                Group = groups.Navigation
+            },
+            new NavItem()
+            {
+                Name = "Table",
+                Url = "Table",
+                Group = groups.DataTable
+            },
+            new NavItem()
+            {
+                Name = "Paginator",
+                Url = "Paginator",
+                Group = groups.DataTable
+            },
+            new NavItem()
+            {
+                Name = "SortHeader",
+                Url = "SortHeader",
+                Group = groups.DataTable
+            },
+            // new NavItem()
+            // {
+            //     Name = "DataTable",
+            //     Url = "DataTable",
+            //     Group = groups.DataTable
+            // },
+            new NavItem()
+            {
+                Name = "BaseComponent",
+                Url = "BaseComponent",
+                Group = groups.Other
+            },
+            new NavItem()
+            {
+                Name = "ForwardRef & RefBack",
+                Url = "ForwardRef",
+                Group = groups.Other
+            },
+            new NavItem()
+            {
+                Name = "Virtual Scroll",
+                Url = "VirtualScroll",
+                Group = groups.Layout
+            },
+
+            new NavItem()
+            {
+                Name = "ForwardRefContext",
+                Url = "ForwardRefContext",
+                Group = groups.Other
+            },
+
+            new NavItem()
+            {
+                Name = "DialogService",
+                Url = "DialogService",
+                Group = groups.PopupsAndModals
+            },
+    // new NavItem()
+    // {
+    //     Name = "Test",
+    //     Url = "Test",
+    //     Group = groups.Other
+    // },
+        };
+            return navItems;
+        }
+    }
+}
--- src/MatBlazor.Doc/Models/NavModel.cs
+++ src/MatBlazor.Doc/Models/NavModel.cs
@@ -1,13 +1,35 @@
 ﻿namespace MatBlazor.Demo.Models
 {
+    public class NavBarLinkModel
+    {
+        public string Url { get;  }
+        public string Text { get; }
+        public NavBarLinkModel(string url,string text, string icon)
+        {
+            Url = url;
+            Text = text;
+            Icon = icon;
+        }
+
+        public string Icon { get;  }
+
+        public static implicit operator NavBarLinkModel((string url, string text, string icon) x)
+        {
+            return new NavBarLinkModel(x.url, x.text, x.icon);
+        }
+    }
     public class NavModel
     {
         public string Title { get;  }
 
-        public NavModel(string title)
+        public NavModel(string title, params NavBarLinkModel[] navBarLinks)
         {
             Title = title;
+            this.NavBarLinks = navBarLinks;
         }
+
+        public  NavBarLinkModel[] NavBarLinks { get;  }
+
 
         public NavGroupModel[] NavGroups { get; set; }
     }
